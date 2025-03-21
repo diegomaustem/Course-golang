d4b6476b2c95cbab4bf1d9756bd6b0320aaf9486
maustem <diego.maustem@gmail.com>
2025-03-20 23:18:45 -0300

    :sparkles: feat: Implementing loops first part

---
+++ 14 - Loops/loops.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	// Is normal :::
+	number := 0
+
+	for number < 10 {
+		number++
+		fmt.Println(number)
+	}
+
+	// Increment ::: 
+	for j := 0; j < 10; j++ {
+	 	fmt.Println("Increment",j)
+	 	time.Sleep(time.Second)
+	}
+
+	// Using range :::
+	names := [5]string{"Carlos", "Marcelo", "Pedro", "Larissa", "Leticia"}
+	for _, name := range names {
+		fmt.Println(name)
+	}
+
+	// Iterate for letter ::: 
+	for _, letter := range "Beet" {
+		fmt.Println(string(letter))
+	}
+}
