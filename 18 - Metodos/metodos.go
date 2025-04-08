cfaa5af0991c128221f37f2084b8a16200df1c65
diegomaustem <diego.maustem@gmail.com>
2025-04-08 09:48:47 -0300

    :sparkles: feat: Implementing method verify of age

--- 18 - Metodos/metodos.go
+++ 18 - Metodos/metodos.go
@@ -11,11 +11,25 @@ func (u user) userSave() {
 	fmt.Printf("Saved user: %s\n", u.name)
 }
 
+func (u *user) haveABirthday() {
+	u.age++
+}
+
+func (u user) ofAge() bool{
+	if u.age >= 18 {
+		return true
+	}
+	return false
+}
+
 func main() {
 	newUser := user{
 		name: "Lucas",
-		age: 25,
+		age: 12,
 	}
 	newUser.userSave()
+	newUser.haveABirthday()
+	ofAge := newUser.ofAge()
 	fmt.Println(newUser)	
+	fmt.Println(ofAge)
 }
